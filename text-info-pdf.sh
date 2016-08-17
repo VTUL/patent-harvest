c579496e881e5d9bcb260c195820840004320613
Philippe Gray <philippe@vt.edu>
2016-08-17 12:23:01 -0400

    Switched to pdfsandwich

--- text-info-pdf.sh
+++ text-info-pdf.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
 cd filedump
+
 for f in *.pdf
 
 do
-
-    mkdir $f".tmp"
-    cd $f".tmp"
-    pdfimages -j "../"$f ./
-    convert \*.pbm -type Grayscale \%d.tif
-    rm -- \*.pbm
-    
-    for tifimg in *.tif
-    do
-        tesseract -l eng $tifimg $tifimg pdf
-    done
-    
-    pdfunite  $(ls *.pdf | sort -n) output.pdf
-    mv output.pdf "../with_text_"$f
-    
-    cd ..
-    rm -rf *.tmp
-
-
+    printf "\n[INFO] Processing file %d\n" "$count"
+    ../pdfsandwich -quiet $f
+    ((count++))
 done
 
 
